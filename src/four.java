ec3eff75aba064bd5b080bc6a1fcd00264d372d5
swapnil <swapnil@123gmail.com>
2022-02-03 23:56:09 +0530

    QA2

---
+++ src/four.java
@@ -0,0 +1,8 @@
+
+public class four 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("shubham");
+	}
+}
---
+++ src/three.java
@@ -0,0 +1,7 @@
+
+public class three {
+	public static void main(String[] args) 
+	{
+		System.out.println("prashant");
+	}
+}
